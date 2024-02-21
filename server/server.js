0d0b39607175fcebf2f2ada6156b4d56a462a2c1
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-21 20:18:11 +0530

    added a new route for the registration page

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.status(200).send("Welcome to world best mern series by thapa technical");
 });
 
+app.get("/register", (req, res) => {
+  res.status(200).send("Welcome to registration page");
+});
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`server is running at port: ${PORT}`);
