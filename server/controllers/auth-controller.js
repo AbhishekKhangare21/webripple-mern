9d4a43080aeb98ce30c9b410bac3ac6af84994aa
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-22 19:53:41 +0530

    add user registration functionality

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user-model");
+
 const home = async (req, res) => {
   try {
     res
@@ -15,7 +17,19 @@ const home = async (req, res) => {
 const register = async (req, res) => {
   try {
     console.log(req.body);
-    res.status(400).send({ massage: req.body });
+    const { username, email, phone, password } = req.body;
+
+    const userExist = await User.findOne({ email: email });
+
+    console.log("userExist ===>", userExist);
+
+    if (userExist) {
+      return res.status(400).json({ msg: "email already exists" });
+    }
+
+    const userCreated = await User.create({ username, email, phone, password });
+
+    res.status(200).send({ userCreated });
   } catch (error) {
     res.status(400).send({ msg: "page not found" });
   }
