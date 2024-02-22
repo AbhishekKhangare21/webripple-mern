7307d444b9aeb5c6d6a1b07c8637e41bb50b244e
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-22 17:01:21 +0530

    update dependencies and add MongoDB and Mongoose

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
+const connectDb = require("./utils/db");
 
 app.use(express.json());
 app.use("/api/auth", router);
 
 const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`server is running at port: ${PORT}`);
-});
 
-// lecture 6 - 10:24 start
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`);
+  });
+});
